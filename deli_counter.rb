7aec02913154da5cf198846b1edee59fa2e5d79c
Jarrian McLean <jarrian.mclean@gmail.com>
2018-11-05 22:44:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    message = "the line is currently:"
+    array.each do |value,index|
+      message += "#{index.to_i+1}. #{value}"
+    end
+    puts "#{message}"
+  end
+end
