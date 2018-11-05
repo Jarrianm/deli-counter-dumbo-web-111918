0728fd5fd517984f255a3de5b3096358921191fc
Jarrian McLean <jarrian.mclean@gmail.com>
2018-11-05 22:49:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,13 @@
 def line(array)
-  if array.size == 0
-    puts "The line is currently empty."
-  else
-    message = "The line is currently: "
-    array.each do |value,index|
-      message += "#{index.to_i+1}. #{value}"
+  if array.length >= 1
+      nuarray = []
+      counter = 1
+      array.each do |name|
+        nuarray.push("#{counter}. #{name}")
+        counter += 1
+      end
+      puts "The line is currently: #{nuarray.join(" ")}"
+    else
+      puts "The line is currently empty."
     end
-    puts "#{message}"
   end
-end
